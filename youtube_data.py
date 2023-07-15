f52b64953ce110feb733e418188a612b6191e2b8
James Mu <jamesmu365@gmail.com>
2023-07-15 15:44:27 -0700

    Fixed typos

---
+++ youtube_data.py
@@ -0,0 +1,17 @@
+import personal
+
+from googleapiclient.discovery import build
+
+api_service_name = 'youtube'
+api_version = 'v3'
+
+youtube = build(api_service_name, api_version, developerKey=personal.yt_api_key)
+
+request = youtube.channels().list(
+        part='statistics',
+        id=personal.yt_id
+    )
+
+response = request.execute()
+
+print(response)
