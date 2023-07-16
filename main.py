a8a2dba9b2baafdcdc868cd6fd6a69f55677efb5
James Mu <jamesmu365@gmail.com>
2023-07-15 17:49:07 -0700

    get_playlist_data added

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import youtube_playlist as yp
 import personal
 
 if __name__ == "__main__":
-    print(yp.get_all_channel_public_playlists(personal.yt_api_key, personal.yt_channel_id))
+    print(yp.get_all_channel_public_playlists(personal.yt_channel_id, personal.yt_api_key))
+    print(yp.get_playlist_data(personal.yt_playlist_id, personal.yt_api_key))
