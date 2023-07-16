0eb8b9c57ca3ca18def1b11da3768d5763bb9391
James Mu <jamesmu365@gmail.com>
2023-07-16 08:46:06 -0700

    Added get_subscriptions_at_datetime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import youtube_channel as yc
 import youtube_playlist as yp
+import youtube_subscription as ys
 import personal
 
 if __name__ == "__main__":
@@ -7,3 +8,4 @@ if __name__ == "__main__":
     print(yp.get_all_channel_public_playlists(personal.yt_channel_id, personal.yt_api_key))
     print(yp.get_playlist_data(personal.yt_playlist_id, personal.yt_api_key))
     yp.print_playlist_video_titles(personal.yt_playlist_id, personal.yt_api_key)
+    print(ys.get_subscriptions_at_datetime(personal.yt_channel_id, personal.yt_api_key))
