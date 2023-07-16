449f62310883f0a5514fdec1d8e0ff33fef49ddb
James Mu <jamesmu365@gmail.com>
2023-07-16 08:35:15 -0700

    Added get_channel_statistics

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import youtube_playlist as yp
 import personal
 
 if __name__ == "__main__":
+    print(yc.get_channel_statisitcs(personal.yt_channel_id, personal.yt_api_key))
     print(yp.get_all_channel_public_playlists(personal.yt_channel_id, personal.yt_api_key))
     print(yp.get_playlist_data(personal.yt_playlist_id, personal.yt_api_key))
     yp.print_playlist_video_titles(personal.yt_playlist_id, personal.yt_api_key)
