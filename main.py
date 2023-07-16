0c1e13c42a31a65825f9f14965679bf40ee4e1c4
James Mu <jamesmu365@gmail.com>
2023-07-15 18:28:07 -0700

    Added print_playlist_video_titles

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ import personal
 if __name__ == "__main__":
     print(yp.get_all_channel_public_playlists(personal.yt_channel_id, personal.yt_api_key))
     print(yp.get_playlist_data(personal.yt_playlist_id, personal.yt_api_key))
+    yp.print_playlist_video_titles(personal.yt_playlist_id, personal.yt_api_key)
