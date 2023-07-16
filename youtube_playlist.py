a5b119883c152ca82252f7b9586891525ccdcc5c
James Mu <jamesmu365@gmail.com>
2023-07-15 17:29:03 -0700

    Initial youtube_playlist functionality

--- youtube_playlist.py
+++ youtube_playlist.py
@@ -1,1 +1,20 @@
-import personal.py
+import personal
+
+from googleapiclient.discovery import build
+
+api_service_name = 'youtube'
+api_version = 'v3'
+
+def get_all_channel_public_playlists(yt_api_key, yt_channel_id):
+
+    youtube = build(api_service_name, api_version, developerKey=yt_api_key)
+
+    request = youtube.playlists().list(
+        part="contentDetails",
+        channelId=yt_channel_id,
+        maxResults=25
+    )
+
+    response = request.execute()
+
+    return response
