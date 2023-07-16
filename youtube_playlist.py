923e4163613f0ec8e1155ed1aec6ccc51ac4fee1
James Mu <jamesmu365@gmail.com>
2023-07-15 18:28:27 -0700

    Working print_playlist_video_titles implementation

--- youtube_playlist.py
+++ youtube_playlist.py
@@ -31,3 +31,35 @@ def get_playlist_data(playlist_id, yt_api_key=personal.yt_api_key):
     response = request.execute()
 
     return response
+
+def print_playlist_video_titles(playlist_id, yt_api_key=personal.yt_api_key):
+
+    youtube = build(api_service_name, api_version, developerKey=yt_api_key)
+
+    request = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=playlist_id,
+        maxResults=5
+    )
+
+    response = request.execute()
+
+    playlistItems = response['items']
+    nextPageToken = response.get('nextPageToken')
+
+    while nextPageToken:
+        request = youtube.playlistItems().list(
+            part="snippet",
+            playlistId=playlist_id,
+            maxResults=5,
+            pageToken=nextPageToken
+        )
+
+        response = request.execute()
+
+        playlistItems.extend(response['items'])
+        nextPageToken = response.get('nextPageToken')
+
+    for video in playlistItems:
+        print(video['snippet']['title'])
+
