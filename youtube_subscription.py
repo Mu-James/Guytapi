082108a975be6e36f12cd9a545f2024eb2e0d98f
James Mu <jamesmu365@gmail.com>
2023-07-16 08:49:09 -0700

    Small comment

--- youtube_subscription.py
+++ youtube_subscription.py
@@ -4,6 +4,7 @@ from googleapiclient.discovery import build
 api_service_name = 'youtube'
 api_version = 'v3'
 
+#Subscriptions must not be set to private to work
 def get_subscriptions_at_datetime(yt_channel_id, yt_api_key=personal.yt_api_key):
 
     youtube = build(api_service_name, api_version, developerKey=yt_api_key)
