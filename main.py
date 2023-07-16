0bc33c9cf9f4cbc4ef7748796e06ff8e7a8d541f
James Mu <jamesmu365@gmail.com>
2023-07-16 08:49:22 -0700

    Renamed to get_subscriptions_with_datetime

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     print(yp.get_all_channel_public_playlists(personal.yt_channel_id, personal.yt_api_key))
     print(yp.get_playlist_data(personal.yt_playlist_id, personal.yt_api_key))
     yp.print_playlist_video_titles(personal.yt_playlist_id, personal.yt_api_key)
-    print(ys.get_subscriptions_at_datetime(personal.yt_channel_id, personal.yt_api_key))
+    print(ys.get_subscriptions_with_datetime(personal.yt_channel_id, personal.yt_api_key))
