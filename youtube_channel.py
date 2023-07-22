b67dadfab8a87cef58e26a219ff88c6fcd4dde8b
James Mu <jamesmu365@gmail.com>
2023-07-22 13:48:17 -0700

    Now using build_youtube

--- youtube_channel.py
+++ youtube_channel.py
@@ -1,11 +1,8 @@
 import personal
-from googleapiclient.discovery import build
+from build import build_youtube
 
 def get_channel_statisitcs(yt_channel_id, yt_api_key=personal.yt_api_key):
-    api_service_name = 'youtube'
-    api_version = 'v3'
-
-    youtube = build(api_service_name, api_version, developerKey=yt_api_key)
+    youtube = build_youtube(yt_api_key)
 
     request = youtube.channels().list(
             part='statistics',
