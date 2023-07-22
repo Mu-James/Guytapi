23bcde0599f81b56eccdc12f58f0f8b0b6926335
James Mu <jamesmu365@gmail.com>
2023-07-22 14:35:00 -0700

    Initial get_video_view_count implementation

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import youtube_video as yv
 import personal
 
 if __name__ == "__main__":
+
     print(yc.get_channel_statisitcs(personal.yt_channel_id, personal.yt_api_key))
     print(yp.get_all_channel_public_playlists(personal.yt_channel_id, personal.yt_api_key))
     print(yp.get_playlist_data(personal.yt_playlist_id, personal.yt_api_key))
-    yp.print_playlist_video_titles(personal.yt_playlist_id, personal.yt_api_key)
-    print(ys.get_subscriptions_with_datetime(personal.yt_channel_id, personal.yt_api_key))
+    #yp.print_playlist_video_titles(personal.yt_playlist_id, personal.yt_api_key)
+    #print(ys.get_subscriptions_with_datetime(personal.yt_channel_id, personal.yt_api_key))
     print(yv.get_video_thumbnail_url('cwFL2ICfj0A', 'default', personal.yt_api_key))
+    print(yv.get_video_view_count('cwFL2ICfj0A', personal.yt_api_key))
+    
--- youtube_video.py
+++ youtube_video.py
@@ -13,3 +13,14 @@ def get_video_thumbnail_url(video_id, size, yt_api_key=personal.yt_api_key):
     video = response['items']
     return video[0]['snippet']['thumbnails'][size]['url']
 
+def get_video_view_count(video_id, yt_api_key=personal.yt_api_key):
+    youtube = build_youtube(yt_api_key)
+    
+    request = youtube.videos().list(
+        part="statistics",
+        id=video_id
+    )
+
+    response = request.execute()
+    video = response['items']
+    return video[0]['statistics']['viewCount']
