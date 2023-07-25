870650674dfd0dfdd533e2635ed4edf6b8b4d13e
James Mu <jamesmu365@gmail.com>
2023-07-25 15:10:28 -0700

    Added and implemented constants

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 from googleapiclient.discovery import build
 
+YT_SERVICE = "youtube"
+YT_VERSION = 'v3'
+
+
 def build_youtube(yt_api_key):
-    api_service_name = 'youtube'
-    api_version = 'v3'
+    api_service_name = YT_SERVICE
+    api_version = YT_VERSION
 
     return build(api_service_name, api_version, developerKey=yt_api_key)
