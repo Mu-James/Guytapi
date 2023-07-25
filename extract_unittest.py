87a5b95e4d48d6375a92862f96096e44ce4d2733
James Mu <jamesmu365@gmail.com>
2023-07-24 18:41:20 -0700

    Added TestExtractMethods

--- extract_unittest.py
+++ extract_unittest.py
@@ -1,5 +1,13 @@
 import unittest as ut
-import extract
+import extract as e
+
+
+class TestExtractMethods(ut.TestCase):
+
+    def test_generate_url(self):
+        e._generate_request("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        self.assertEqual(True, True)
+
 
 if __name__ == "__main__":
-    pass
+    ut.main()
