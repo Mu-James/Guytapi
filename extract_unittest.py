a1ca0fb998d66623cd9db5e30a4a82441a4ad644
James Mu <jamesmu365@gmail.com>
2023-07-24 19:28:19 -0700

    Added TestGetHost unittests

--- extract_unittest.py
+++ extract_unittest.py
@@ -2,8 +2,7 @@ import unittest as ut
 import extract as e
 
 
-class TestExtractMethods(ut.TestCase):
-
+class TestGeneratetUrl(ut.TestCase):
     def test_generate_url_general(self):
         e._generate_request("https://www.google.com/")
         self.assertEqual(True, True)
@@ -28,10 +27,30 @@ class TestExtractMethods(ut.TestCase):
         e._generate_request("https://www.youtube.com/@YouTube")
         self.assertEqual(True, True)
 
-    def test_generate_url_invalid_general(self):
+    def test_generate_url_general_invalid(self):
         with self.assertRaises(Exception):
             e._generate_request("google")
         
+class TestGetHost(ut.TestCase):
+    def test_get_host_general(self):
+        self.assertEqual(e._get_host("https://github.com"), "github.com")
+
+    def test_get_host_general_subdirectory(self):
+        self.assertEqual(e._get_host("https://github.com/login"), "github.com")
+
+    def test_get_host_yt(self):
+        self.assertEqual(e._get_host("https://www.youtube.com/"), "www.youtube.com")
+
+    def test_get_host_yt_video(self):
+        self.assertEqual(e._get_host("https://www.youtube.com/watch?v=Iz3K7wkJx4E"), "www.youtube.com")
+
+    def test_get_host_yt_video_shortened(self):
+        self.assertEqual(e._get_host("https://youtu.be/Iz3K7wkJx4E"), "youtu.be")
+
+    def test_get_host_general_invalid(self):
+        with self.assertRaises(Exception):
+            e._get_host("google")        
 
 if __name__ == "__main__":
     ut.main()
+    
