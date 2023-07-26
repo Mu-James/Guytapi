1105464ed7caf856080c4e299618036bd44dfb0a
James Mu <jamesmu365@gmail.com>
2023-07-25 17:07:35 -0700

    Added url versions of view count and thumbnail

--- youtube_video.py
+++ youtube_video.py
@@ -1,7 +1,8 @@
 import personal
+import extract as e
 from build import build_youtube
 
-def get_video_thumbnail_url(video_id, size, yt_api_key=personal.yt_api_key):
+def get_video_thumbnail_url_id(video_id, size, yt_api_key=personal.yt_api_key):
     youtube = build_youtube(yt_api_key)
 
     request = youtube.videos().list(
@@ -13,7 +14,7 @@ def get_video_thumbnail_url(video_id, size, yt_api_key=personal.yt_api_key):
     video = response['items']
     return video[0]['snippet']['thumbnails'][size]['url']
 
-def get_video_view_count(video_id, yt_api_key=personal.yt_api_key):
+def get_video_view_count_id(video_id, yt_api_key=personal.yt_api_key):
     youtube = build_youtube(yt_api_key)
     
     request = youtube.videos().list(
@@ -24,3 +25,11 @@ def get_video_view_count(video_id, yt_api_key=personal.yt_api_key):
     response = request.execute()
     video = response['items']
     return video[0]['statistics']['viewCount']
+
+def get_video_view_count_url(url, yt_api_key):
+    video_id = e.extract_youtube_video_id_from_url(url)
+    return get_video_view_count_id(video_id, yt_api_key)
+
+def get_video_thumbnail_url_url(url, yt_api_key):
+    video_id = e.extract_youtube_video_id_from_url(url)
+    return get_video_thumbnail_url_id(video_id, yt_api_key)
