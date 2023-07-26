44102d3328ab4be4114c5fbae5ba85089018852c
James Mu <jamesmu365@gmail.com>
2023-07-25 17:34:30 -0700

    Implemented _is_yt_host

--- extract.py
+++ extract.py
@@ -27,11 +27,14 @@ def _get_playlist_id(query):
 def _get_url_query(url):
     return urlparse(url).query
 
+def _is_yt_host(host):
+    return host == YT_URL or host == YT_URL_SHORT
+
 def extract_youtube_video_id_from_url(url):
     try:
         host = _get_host(url)
 
-        if host == YT_URL or host == YT_URL_SHORT:
+        if _is_yt_host(host):
             return _get_video_id(_get_url_query(url))
         else:
             raise NotYoutubeHostException
@@ -43,7 +46,7 @@ def extract_youtube_playlist_id_from_url(url):
     try:
         host = _get_host(url)
 
-        if host == YT_URL or host == YT_URL_SHORT:
+        if _is_yt_host(host):
             return _get_playlist_id(_get_url_query(url))
         else:
             raise NotYoutubeHostException
