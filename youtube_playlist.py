227e10636c3143540c2b8fac6288ee0b13e85f67
James Mu <jamesmu365@gmail.com>
2023-07-25 17:31:12 -0700

    Added methods

--- youtube_playlist.py
+++ youtube_playlist.py
@@ -57,3 +57,9 @@ def print_playlist_video_titles_using_id(playlist_id, yt_api_key=personal.yt_api
     for video in playlistItems:
         print(video['snippet']['title'])
 
+def get_all_channel_public_playlists_using_url(url, yt_api_key)
+    pass
+
+def get_playlist_data_using_url(url, yt_api_key):
+    playlist_id = e.extract_youtube_playlist_id_from_url(url)
+    return get_playlist_data_using_id(playlist_id, yt_api_key)
