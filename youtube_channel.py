e344a1e0fa108eb84f2caa1d245c75bcdddf15a7
James Mu <jamesmu365@gmail.com>
2023-07-27 15:56:16 -0700

    get_channel_statistics_url

--- youtube_channel.py
+++ youtube_channel.py
@@ -1,7 +1,8 @@
 import personal
 from build import build_youtube
+import extract as e
 
-def get_channel_statisitcs(yt_channel_id, yt_api_key=personal.yt_api_key):
+def get_channel_statisitcs_id(yt_channel_id, yt_api_key=personal.yt_api_key):
     youtube = build_youtube(yt_api_key)
 
     request = youtube.channels().list(
@@ -12,3 +13,7 @@ def get_channel_statisitcs(yt_channel_id, yt_api_key=personal.yt_api_key):
     response = request.execute()
 
     return response
+
+def get_channel_statistics_url(url, yt_api_key):
+    channel_id = e.extract_youtube_channel_id_from_url(url)
+    return get_channel_statisitcs_id(channel_id, yt_api_key)
