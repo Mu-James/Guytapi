c7102a5139561d5d63fecb6f63828683c5a0fe4b
James Mu <jamesmu365@gmail.com>
2023-07-30 08:54:14 -0700

    Initial implementation and rename

--- view.py
+++ view.py
@@ -1,8 +1,18 @@
 import tkinter as tk
 
-class YoutubeApiApp:
+_BUTTON_API_RELATED = "API Related"
+_BUTTON_NON_API_RELATED = "Non-API Related"
+_WINDOW_TITLE = "Youtube API Application"
+_START_RESOLUTION = "640x640"
+
+class YoutubeApiGUI:
     def __init__(self):
         self._root_window = tk.Tk()
+        self._root_window.title(_WINDOW_TITLE)
     
     def run(self):
         self._root_window.mainloop()
+
+if __name__ == "__main__":
+    ytapigui = YoutubeApiGUI()
+    ytapigui.run()
