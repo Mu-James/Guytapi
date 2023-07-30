518e4827757348e02d3e2b105f459e235acca706
James Mu <jamesmu365@gmail.com>
2023-07-30 09:16:06 -0700

    Added some buttons

--- view.py
+++ view.py
@@ -1,17 +1,34 @@
 import tkinter as tk
 
-_BUTTON_API_RELATED = "API Related"
-_BUTTON_NON_API_RELATED = "Non-API Related"
+_BUTTON_TEXT_API_RELATED = "API Related"
+_BUTTON_TEXT_NON_API_RELATED = "Non-API Related"
 _WINDOW_TITLE = "Youtube API Application"
-_START_RESOLUTION = "640x640"
+_START_RESOLUTION = "512x512"
+_DEFAULT_FONT = "arial"
 
 class YoutubeApiGUI:
     def __init__(self):
         self._root_window = tk.Tk()
         self._root_window.title(_WINDOW_TITLE)
-    
+        self._root_window.geometry(_START_RESOLUTION)
+
+        #Buttons
+        api_related = self._create_button(self._root_window, _BUTTON_TEXT_API_RELATED, 0, 0)
+        non_api_realted = self._create_button(self._root_window, _BUTTON_TEXT_NON_API_RELATED, 1, 0)
+
     def run(self):
         self._root_window.mainloop()
+
+    def _create_button(self, master, text, row, column):
+        button = tk.Button(
+            master = master,
+            text = text, font = _DEFAULT_FONT,
+        )
+        button.grid(
+            row = row,
+            column = column,
+        )
+        return button
 
 if __name__ == "__main__":
     ytapigui = YoutubeApiGUI()
