32ab03328b3e65c2e8efe18cf8145a55e1ceecb0
James Mu <jamesmu365@gmail.com>
2023-07-30 09:45:02 -0700

    Tweaked _create_button and style

--- view.py
+++ view.py
@@ -1,12 +1,17 @@
 import tkinter as tk
 
 _LABLE_TEXT_WELCOME = "Welcome, please choose an option below:"
+
 _BUTTON_TEXT_API_RELATED = "API Related"
 _BUTTON_TEXT_NON_API_RELATED = "Non-API Related"
+_BUTTON_TEXT_CLOSE = "Close"
+
 _WINDOW_TITLE = "Youtube API GUI"
 _START_RESOLUTION = "512x512"
 _DEFAULT_FONT = "Arial"
 
+def _dummy():
+    pass
 
 class YoutubeApiGUI:
     def __init__(self):
@@ -18,22 +23,25 @@ class YoutubeApiGUI:
         welcome = self._create_label(self._root_window, _LABLE_TEXT_WELCOME, 0, 0)
 
         #Buttons
-        api_related = self._create_button(self._root_window, _BUTTON_TEXT_API_RELATED, 1, 0)
-        non_api_realted = self._create_button(self._root_window, _BUTTON_TEXT_NON_API_RELATED, 2, 0)
+        api_related = self._create_button(self._root_window, _BUTTON_TEXT_API_RELATED, 1, 0, _dummy)
+        non_api_realted = self._create_button(self._root_window, _BUTTON_TEXT_NON_API_RELATED, 2, 0, _dummy)
+        close = self._create_button(self._root_window, _BUTTON_TEXT_CLOSE, 3, 0, self._root_window.quit)
 
     def run(self):
         self._root_window.mainloop()
 
-    def _create_button(self, master, text, row, column):
+    def _create_button(self, master, text, row, column, command):
         button = tk.Button(
             master = master,
             text = text, font = _DEFAULT_FONT,
+            command = command
         )
         button.grid(
             row = row,
             column = column,
         )
         return button
+    
     def _create_label(self, master, text, row, column):
         label = tk.Label(
             master = master,
