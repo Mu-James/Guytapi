eb01bf4887388fccb460b09e24ae09fdc9c56cd9
James Mu <jamesmu365@gmail.com>
2023-07-30 09:32:41 -0700

    Welcome text

--- view.py
+++ view.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
+_LABLE_TEXT_WELCOME = "Welcome, please choose an option below:"
 _BUTTON_TEXT_API_RELATED = "API Related"
 _BUTTON_TEXT_NON_API_RELATED = "Non-API Related"
 _WINDOW_TITLE = "Youtube API GUI"
 _START_RESOLUTION = "512x512"
 _DEFAULT_FONT = "Arial"
+
 
 class YoutubeApiGUI:
     def __init__(self):
@@ -12,9 +14,12 @@ class YoutubeApiGUI:
         self._root_window.title(_WINDOW_TITLE)
         self._root_window.geometry(_START_RESOLUTION)
 
+        #Labels
+        welcome = self._create_label(self._root_window, _LABLE_TEXT_WELCOME, 0, 0)
+
         #Buttons
-        api_related = self._create_button(self._root_window, _BUTTON_TEXT_API_RELATED, 0, 0)
-        non_api_realted = self._create_button(self._root_window, _BUTTON_TEXT_NON_API_RELATED, 1, 0)
+        api_related = self._create_button(self._root_window, _BUTTON_TEXT_API_RELATED, 1, 0)
+        non_api_realted = self._create_button(self._root_window, _BUTTON_TEXT_NON_API_RELATED, 2, 0)
 
     def run(self):
         self._root_window.mainloop()
@@ -32,7 +37,9 @@ class YoutubeApiGUI:
     def _create_label(self, master, text, row, column):
         label = tk.Label(
             master = master,
-            text = text
+            text = text,
+        )
+        label.grid(
             row = row,
             column = column
         )
