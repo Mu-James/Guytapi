f639645d1343e9e79900b6896a84bc6344022a78
James Mu <jamesmu365@gmail.com>
2023-09-04 08:38:13 -0700

    Removed unnecessary imports

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import youtube_channel as yc
-import youtube_playlist as yp
-import youtube_subscription as ys
-import youtube_video as yv
-import extract as e
 import gui as g
-import personal
 
 if __name__ == "__main__":
     ytgui = g.YoutubeApiGUI()
