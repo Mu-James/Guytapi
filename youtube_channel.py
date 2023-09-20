2810e3957861c8af20416ff268e78c7680ba1267
James Mu <jamesmu365@gmail.com>
2023-09-20 13:39:20 -0700

    get_parsed_channel_statistics_response

--- youtube_channel.py
+++ youtube_channel.py
@@ -17,3 +17,15 @@ def get_channel_statisitcs_id(yt_channel_id, yt_api_key=personal.yt_api_key):
 def get_channel_statistics_url(url, yt_api_key):
     channel_id = e.extract_youtube_channel_id_from_url(url)
     return get_channel_statisitcs_id(channel_id, yt_api_key)
+
+def get_parsed_channel_statistics_response(channel_statistics_response):
+    stats = channel_statistics_response['items'][0]['statistics']
+
+    parsed = f"View Count: {stats['viewCount']}\n"
+
+    if stats['hiddenSubscriberCount'] != True:
+        parsed += f"Subscriber Count: {stats['subscriberCount']}\n"
+
+    parsed += f"Video Count: {stats['videoCount']}"
+
+    return parsed
